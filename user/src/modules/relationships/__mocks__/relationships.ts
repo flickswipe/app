ced1a05d1587fdd2a5fc522265a35e43a5943bb6
Chris Drifter <developer@kolaps.com>
2020-10-31 14:56:46 +0100

    Mock relationships

---
+++ user/src/modules/relationships/__mocks__/relationships.ts
@@ -0,0 +1,6 @@
+export const acceptRelationship = jest.fn().mockResolvedValue(null);
+export const blockRelationship = jest.fn().mockResolvedValue(null);
+export const cancelRelationship = jest.fn().mockResolvedValue(null);
+export const rejectRelationship = jest.fn().mockResolvedValue(null);
+export const requestRelationship = jest.fn().mockResolvedValue(null);
+export const unblockRelationship = jest.fn().mockResolvedValue(null);
