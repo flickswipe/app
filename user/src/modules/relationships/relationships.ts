f30819448c10fcf8722dfe7b650fa6d0c4dd3159
Chris Drifter <developer@kolaps.com>
2020-10-31 14:12:14 +0100

    Test relationship flows

--- user/src/modules/relationships/relationships.ts
+++ user/src/modules/relationships/relationships.ts
@@ -1,5 +1,6 @@
 export * from "./services/accept-relationship";
 export * from "./services/block-relationship";
+export * from "./services/cancel-relationship";
 export * from "./services/request-relationship";
 export * from "./services/reject-relationship";
 export * from "./services/unblock-relationship";
