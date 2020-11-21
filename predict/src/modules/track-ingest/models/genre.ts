2bb78bded2099d056b1b27ea50c8c57a161c86de
Chris Drifte <chrisdrifte@gmail.com>
2020-11-21 17:33:50 +0100

    Split dumb nodejs "predict" microservice from "survey"

---
+++ predict/src/modules/track-ingest/models/genre.ts
@@ -0,0 +1,84 @@
+import mongoose from "mongoose";
+import { iso6391 } from "@flickswipe/common";
+
+/**
+ * Properties used to create a Genre
+ */
+interface GenreAttrs {
+  id: string;
+  name: string;
+  language: iso6391;
+}
+
+/**
+ * Properties that a MovieId document has
+ */
+interface GenreDoc extends mongoose.Document {
+  id: string;
+  name: string;
+  language: iso6391;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+/**
+ * Genre mongoose schema
+ */
+const genreSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    language: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        delete ret._id;
+        delete ret.__v;
+        delete ret.language;
+        delete ret.createdAt;
+        delete ret.updatedAt;
+      },
+    },
+  }
+);
+
+/**
+ * Build wrapper (used to enforce GenreAttrs)
+ */
+interface GenreModel extends mongoose.Model<GenreDoc> {
+  build(attrs: GenreAttrs): GenreDoc;
+  id(string: string): mongoose.Types.ObjectId;
+}
+
+genreSchema.statics.build = (attrs: GenreAttrs) => {
+  return new Genre(
+    Object.assign({ _id: genreSchema.statics.id(attrs.id) }, attrs)
+  );
+};
+
+genreSchema.statics.id = (string = "") => {
+  return string
+    ? mongoose.Types.ObjectId(string.padStart(24, "0").slice(-24))
+    : mongoose.Types.ObjectId();
+};
+
+/**
+ * Initialize model
+ */
+const Genre = mongoose.model<GenreDoc, GenreModel>("Genre", genreSchema);
+
+/**
+ * Exports
+ */
+export { GenreAttrs, GenreModel, GenreDoc, Genre };
---
+++ predict/src/test/global-helpers.ts
@@ -0,0 +1,39 @@
+/**
+ * Provide global helper functions for tests
+ */
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signIn(email?: string): Promise<string[]>;
+    }
+  }
+}
+
+/**
+ * Simulate a signed in user
+ */
+global.signIn = async (id: string = null, email: string = null) => {
+  // Build a JWT payload.  { id, email }
+  const payload = {
+    id: id || new mongoose.Types.ObjectId().toHexString(),
+    email: email || "test@user.com",
+  };
+
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // return a string thats the cookie with the encoded data
+  return [`express:sess=${base64}`];
+};
