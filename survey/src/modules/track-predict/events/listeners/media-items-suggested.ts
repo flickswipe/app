3b559b6a609db505dc822738dd517752f7d49678
Chris Drifte <chrisdrifte@gmail.com>
2020-11-22 12:21:46 +0100

    Handle bulk media item suggestions

---
+++ survey/src/modules/track-predict/events/listeners/media-items-suggested.ts
@@ -0,0 +1,62 @@
+import {
+  Subjects,
+  Listener,
+  MediaItemsSuggestedEvent,
+} from "@flickswipe/common";
+
+import { Message } from "node-nats-streaming";
+import { Suggestion } from "../../models/suggestion";
+
+const { QUEUE_GROUP_NAME } = process.env;
+
+/**
+ * Listen to `MediaItemsSuggested` events
+ */
+export class MediaItemsSuggestedListener extends Listener<MediaItemsSuggestedEvent> {
+  // set listener subject
+  subject: Subjects.MediaItemsSuggested = Subjects.MediaItemsSuggested;
+
+  // set queue group name
+  queueGroupName = QUEUE_GROUP_NAME;
+
+  /**
+   * @param data message data
+   * @param msg message handler
+   */
+  async onMessage(
+    data: MediaItemsSuggestedEvent["data"],
+    msg: Message
+  ): Promise<void> {
+    // create suggestion doc if none exists
+    ((await Suggestion.findOne(data)) || (await createSuggestionDocs(data))) &&
+      msg.ack();
+  }
+}
+
+/**
+ * @param data data to create with
+ * @returns {boolean} true if message should be acked
+ */
+async function createSuggestionDocs(
+  data: MediaItemsSuggestedEvent["data"]
+): Promise<boolean> {
+  const { user, mediaItems } = data;
+
+  const promises = [];
+
+  try {
+    for (const mediaItem of mediaItems) {
+      const promise = Suggestion.build({ user, mediaItem }).save();
+      promises.push(promise);
+    }
+
+    await Promise.all(promises);
+  } catch (err) {
+    console.error(`Couldn't create suggestion, will try again later.`);
+    console.error(err);
+    return;
+  }
+
+  console.log(`Suggested ${mediaItems.length} media items for user #${user}`);
+  return true;
+}
