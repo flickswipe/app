e738315568dbffb6dc2376e709676bff2a4fbb04
Chris Drifte <chrisdrifte@gmail.com>
2020-11-22 12:55:33 +0100

    Predict tracks user settings

---
+++ predict/src/modules/track-user-settings/events/listeners/__tests__/user-updated-setting.test.ts
@@ -0,0 +1,137 @@
+import {
+  Listener,
+  SettingType,
+  UserUpdatedSettingEvent,
+} from "@flickswipe/common";
+import { Message } from "node-nats-streaming";
+import { natsWrapper } from "../../../../../nats-wrapper";
+import { Setting } from "../../../models/setting";
+import { UserUpdatedSettingListener } from "../user-updated-setting";
+
+const setup = async () => {
+  return {
+    listener: new UserUpdatedSettingListener(natsWrapper.client),
+
+    // @ts-ignore
+    msg: {
+      ack: jest.fn(),
+    } as Message,
+  };
+};
+
+describe("user updated setting listener", () => {
+  let listener: Listener<UserUpdatedSettingEvent>;
+  let msg: Message;
+
+  beforeEach(async () => {
+    const setupValues = await setup();
+    listener = setupValues.listener;
+    msg = setupValues.msg;
+  });
+
+  describe("existing doc", () => {
+    beforeEach(async () => {
+      await Setting.build({
+        settingType: SettingType.Country,
+        user: "aaabbbcccddd",
+        value: "us",
+      }).save();
+    });
+
+    it("should ignore old data", async () => {
+      await listener.onMessage(
+        {
+          settingType: SettingType.Country,
+          user: "aaabbbcccddd",
+          value: "uk",
+          updatedAt: new Date(new Date().getTime() - 86600),
+        },
+        msg
+      );
+
+      expect(
+        await Setting.findOne({
+          settingType: SettingType.Country,
+          user: "aaabbbcccddd",
+        })
+      ).toEqual(
+        expect.objectContaining({
+          value: "us",
+        })
+      );
+    });
+    it("should overwrite doc", async () => {
+      await listener.onMessage(
+        {
+          settingType: SettingType.Country,
+          user: "aaabbbcccddd",
+          value: "uk",
+          updatedAt: new Date(new Date().getTime() + 86600),
+        },
+        msg
+      );
+
+      expect(
+        await Setting.findOne({
+          settingType: SettingType.Country,
+          user: "aaabbbcccddd",
+        })
+      ).toEqual(
+        expect.objectContaining({
+          value: "uk",
+        })
+      );
+    });
+    it("should acknowledge the message", async () => {
+      await listener.onMessage(
+        {
+          settingType: SettingType.Country,
+          user: "aaabbbcccddd",
+          value: "uk",
+          updatedAt: new Date(new Date().getTime() + 86600),
+        },
+        msg
+      );
+
+      expect(msg.ack).toHaveBeenCalled();
+    });
+  });
+
+  describe("create new doc", () => {
+    it("should create a new doc", async () => {
+      await listener.onMessage(
+        {
+          settingType: SettingType.Country,
+          user: "aaabbbcccddd",
+          value: "uk",
+          updatedAt: new Date(new Date().getTime() + 86600),
+        },
+        msg
+      );
+
+      expect(
+        await Setting.findOne({
+          settingType: SettingType.Country,
+          user: "aaabbbcccddd",
+        })
+      ).toEqual(
+        expect.objectContaining({
+          value: "uk",
+        })
+      );
+    });
+    it("should acknowledge the message", async () => {
+      await listener.onMessage(
+        {
+          settingType: SettingType.Country,
+          user: "aaabbbcccddd",
+          value: "uk",
+          updatedAt: new Date(new Date().getTime() + 86600),
+        },
+        msg
+      );
+
+      expect(msg.ack).toHaveBeenCalled();
+    });
+  });
+});
