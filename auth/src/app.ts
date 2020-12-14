77ff1ca56b77589dd12fc54931dd0bce749bfbcb
Chris Drifte <developer@kolaps.com>
2020-12-14 22:35:57 +0100

    Better cookie session options

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -13,6 +13,8 @@ import { usersSendAddEmailLinkRouter } from './routes/user-send-add-email-link';
 import { usersSendMagicLinkRouter } from './routes/user-send-magic-link';
 import { usersSignOutRouter } from './routes/user-sign-out';
 
+const SESSION_LENGTH_MS = 90 * 24 * 60 * 60 * 1000;
+
 /**
  * Configure express app
  */
@@ -29,12 +31,22 @@ app.use(useragent.express());
 app.use(json());
 
 // set cookies on req.session
-app.use(
-  cookieSession({
-    signed: false,
-    secure: process.env.NODE_ENV !== "test",
-  })
-);
+const cookieSessionOptions = {
+  expires: new Date(new Date().getTime() + SESSION_LENGTH_MS),
+  signed: false,
+  secure: false,
+};
+
+if (process.env.NODE_ENV === "production") {
+  Object.assign(cookieSessionOptions, {
+    domain: process.env.HOST,
+    path: "/api/",
+    sameSite: "strict",
+    secure: true,
+  });
+}
+
+app.use(cookieSession(cookieSessionOptions));
 
 /**
  * Configure routes
